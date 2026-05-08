e2f01f059112bb4be18316763ab0dd4e37975dee
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-05-08 23:04:28 +0530

    Add inheritance program

---
+++ BASICS/Inheritance/Company.java
@@ -0,0 +1,46 @@
+package Inheritance;
+
+
+
+class Employee {
+    String name;
+    double salary;
+
+    Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("Employee Name : " + name);
+        System.out.println("Salary : " + salary);
+    }
+}
+
+class Developer extends Employee {
+    String programmingLanguage;
+
+    Developer(String name, double salary, String programmingLanguage) {
+        super(name, salary);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Programming Language : " + programmingLanguage);
+    }
+}
+
+public class Company {
+    public static void main(String[] args) {
+
+        Developer dev1 = new Developer(
+                "Shivashankar",
+                55000,
+                "Java"
+        );
+
+        dev1.displayDetails();
+    }
+}
