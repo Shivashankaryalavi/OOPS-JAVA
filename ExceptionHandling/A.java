57f563ed146970f12017d09e28e22a3082eaf338
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-06-15 12:51:15 +0530

    Add java program to handle arithmeticException

---
+++ ExceptionHandling/A.java
@@ -0,0 +1,20 @@
+package ExceptionHandling;
+
+public class A {
+    public static void main(String[] args) {
+        int a=10,b=0,c;
+       
+     try {
+             c=a/b;
+            System.out.println(c);
+            
+        } catch (ArithmeticException e) {
+            System.out.println("Cant divide by zero");
+        }
+         
+        System.out.println("Hi");
+       
+       
+    }
+    
+}
