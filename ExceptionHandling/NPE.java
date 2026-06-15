e3b45653906157f68890ffca419bfda5ffe88c89
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-06-15 13:17:37 +0530

    Add java program of null pointer exception

---
+++ ExceptionHandling/NPE.java
@@ -0,0 +1,19 @@
+package ExceptionHandling;
+
+public class NPE {
+    public static void main(String[] args) {
+    String str = null;
+    try {
+     System.out.println(str.toUpperCase());
+        
+    } catch (NullPointerException e) {
+        System.out.println("null can't be casted");
+    }
+   
+   
+        
+    }
+ 
+    
+    
+}
